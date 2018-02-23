688b926f74245b25aec851d05b854999191b27f4
Alex Thomson <alexrobthomson@outlook.com>
2018-02-24 10:44:45 +1300

    Update 1.1.0
    Removed Materialize.
    Restructured extension.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-let elements = document.getElementsByTagName("a");
-for (let index = 0; index < elements.length; index++) {
+var elements = document.getElementsByTagName("a");
+for (var index = 0; index < elements.length; index++) {
     if (!elements[index].href) {
         continue;
     }
 
     if (elements[index].hostname === "adfoc.us" && elements[index].search) {
-        let url = elements[index].search.split("url=");
+        var url = elements[index].search.split("&url=");
         if (url.length != 2) {
             continue;
         }
